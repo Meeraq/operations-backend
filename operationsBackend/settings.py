86b7d882d00528ddbc200fb1803b9f5a4219df97
jatin-13-python <jatinchaurasia97@gmail.com>
2023-05-15 17:10:28 +0530

    update allowed origins

--- operationsBackend/settings.py
+++ operationsBackend/settings.py
@@ -160,12 +160,7 @@ SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-	'http://localhost:3001',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:3001',
-]
+CORS_ALLOWED_ORIGINS = [f"{env('APP_URL')}"]
 
 CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
 CORS_ALLOW_CREDENTIALS = True
