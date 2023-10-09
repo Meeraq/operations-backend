ea0c86e8be918e90b429612da2857e06944de15d
jatin-13-python <jatinchaurasia97@gmail.com>
2023-10-09 17:57:49 +0530

    coachee percentage

--- api/views.py
+++ api/views.py
@@ -695,7 +695,6 @@ def create_project_cass(request):
             mode=request.data["mode"],
             sold=request.data["sold"],
             project_description=desc,
-
             # updated_to_sold= request.data['updated_to_sold'],
             location=json.loads(request.data["location"]),
             steps=dict(
@@ -1465,9 +1464,6 @@ def get_projects_and_sessions_by_coach(request, coach_id):
     return Response({"projects": project_serializer.data})
 
 
-
-
-
 # @api_view(["GET"])
 # def coach_session_list(request, coach_id):
 #     projects = Project.objects.filter(coaches_status__coach__id=coach_id)
@@ -1497,9 +1493,6 @@ def get_projects_and_sessions_by_coach(request, coach_id):
 #     return Response({"projects": project_serializer.data})
 
 
-
-
-
 @api_view(["GET"])
 def coach_session_list(request, coach_id):
     projects = Project.objects.filter(coaches_status__coach__id=coach_id)
@@ -1512,7 +1505,7 @@ def coach_session_list(request, coach_id):
     # Group sessions by project ID
     sessions_dict = {}
     for session in session_serializer.data:
-        project_id = session['project']
+        project_id = session["project"]
         if project_id in sessions_dict:
             sessions_dict[project_id].append(session)
         else:
@@ -1520,14 +1513,13 @@ def coach_session_list(request, coach_id):
 
     # Add the session data to the projects
     for project_data in project_serializer.data:
-        project_id = project_data['id']
+        project_id = project_data["id"]
         if project_id in sessions_dict:
-            project_data['sessions'] = sessions_dict[project_id]
+            project_data["sessions"] = sessions_dict[project_id]
         else:
-            project_data['sessions'] = []
+            project_data["sessions"] = []
 
     return Response({"projects": project_serializer.data})
-
 
 
 # @api_view(['POST'])
@@ -3676,17 +3668,29 @@ def get_engagement_in_projects(request, project_id):
     engagements_data = []
 
     for engagement in engagements:
-        completed_sessions_count = SessionRequestCaas.objects.filter(
-            status="completed",
-            project__id=engagement.project.id,
-            learner__id=engagement.learner.id,
-        ).count()
+        completed_sessions_count = (
+            SessionRequestCaas.objects.filter(
+                status="completed",
+                project__id=engagement.project.id,
+                learner__id=engagement.learner.id,
+            )
+            .exclude(
+                Q(billable_session_number__isnull=True) & Q(session_type="chemistry")
+            )
+            .count()
+        )
 
-        total_sessions_count = SessionRequestCaas.objects.filter(
-            project__id=engagement.project.id,
-            learner__id=engagement.learner.id,
-            is_archive=False,
-        ).count()
+        total_sessions_count = (
+            SessionRequestCaas.objects.filter(
+                project__id=engagement.project.id,
+                learner__id=engagement.learner.id,
+                is_archive=False,
+            )
+            .exclude(
+                Q(billable_session_number__isnull=True) & Q(session_type="chemistry")
+            )
+            .count()
+        )
 
         serializer = EngagementDepthOneSerializer(engagement)
         data = serializer.data
@@ -3703,17 +3707,30 @@ def get_engagements_of_hr(request, user_id):
     engagements_data = []
     current_time = int(timezone.now().timestamp() * 1000)
     for engagement in engagements:
-        completed_sessions_count = SessionRequestCaas.objects.filter(
-            status="completed",
-            project__id=engagement.project.id,
-            learner__id=engagement.learner.id,
-        ).count()
+        completed_sessions_count = (
+            SessionRequestCaas.objects.filter(
+                status="completed",
+                project__id=engagement.project.id,
+                learner__id=engagement.learner.id,
+            )
+            .exclude(
+                Q(billable_session_number__isnull=True) & Q(session_type="chemistry")
+            )
+            .count()
+        )
 
-        total_sessions_count = SessionRequestCaas.objects.filter(
-            project__id=engagement.project.id,
-            learner__id=engagement.learner.id,
-            is_archive=False,
-        ).count()
+        total_sessions_count = (
+            SessionRequestCaas.objects.filter(
+                project__id=engagement.project.id,
+                learner__id=engagement.learner.id,
+                is_archive=False,
+            )
+            .exclude(
+                Q(billable_session_number__isnull=True) & Q(session_type="chemistry")
+            )
+            .count()
+        )
+
         upcoming_session = SessionRequestCaas.objects.filter(
             Q(is_booked=True),
             Q(confirmed_availability__end_time__gt=current_time),
@@ -3755,19 +3772,33 @@ class SessionCountsForAllLearners(APIView):
             for engagement in engagements:
                 learner_id = engagement.learner.id
 
-                completed_sessions_count = SessionRequestCaas.objects.filter(
-                    status="completed",
-                    billable_session_number__isnull=False,
-                    learner__id=learner_id,
-                    is_archive=False,
-                ).count()
+                completed_sessions_count = (
+                    SessionRequestCaas.objects.filter(
+                        status="completed",
+                        # billable_session_number__isnull=False,
+                        learner__id=learner_id,
+                        is_archive=False,
+                    )
+                    .exclude(
+                        Q(billable_session_number__isnull=True)
+                        & Q(session_type="chemistry")
+                    )
+                    .count()
+                )
 
-                total_sessions_count = SessionRequestCaas.objects.filter(
-                    learner__id=learner_id,
-                    # project_id = engagement.project_id
-                    billable_session_number__isnull=False,
-                    is_archive=False,
-                ).count()
+                total_sessions_count = (
+                    SessionRequestCaas.objects.filter(
+                        learner__id=learner_id,
+                        # project_id = engagement.project_id
+                        # billable_session_number__isnull=False,
+                        is_archive=False,
+                    )
+                    .exclude(
+                        Q(billable_session_number__isnull=True)
+                        & Q(session_type="chemistry")
+                    )
+                    .count()
+                )
 
                 if learner_id in learners.values_list("id", flat=True):
                     if learner_id not in learner_session_counts:
@@ -5450,41 +5481,56 @@ def get_all_engagements(request):
     # Return the list of serialized engagement data with session counts
     return Response(engagement_data_list)
 
-@api_view(['PUT'])
+
+@api_view(["PUT"])
 def edit_project_caas(request, project_id):
     organisation = Organisation.objects.filter(
         id=request.data["organisation_id"]
     ).first()
-    
+
     try:
         # Retrieve the existing project from the database
         project = get_object_or_404(Project, pk=project_id)
         # Update project attributes based on the data in the PUT request
-        project.name = request.data.get('project_name', project.name)
-        project.approx_coachee = request.data.get('approx_coachee', project.approx_coachee)
-        project.organisation=organisation
-        project.frequency_of_session = request.data.get('frequency_of_session', project.frequency_of_session)
-        project.interview_allowed = request.data.get('interview_allowed', project.interview_allowed)
-        project.specific_coach = request.data.get('specific_coach', project.specific_coach)
-        project.empanelment = request.data.get('empanelment', project.empanelment)
-        project.tentative_start_date = request.data.get('tentative_start_date', project.tentative_start_date)
-        project.mode = request.data.get('mode', project.mode)
-        project.sold = request.data.get('sold', project.sold)
-        project.location = json.loads(request.data.get('location', '[]'))
-        project.project_description = request.data.get('project_description', project.project_description)
+        project.name = request.data.get("project_name", project.name)
+        project.approx_coachee = request.data.get(
+            "approx_coachee", project.approx_coachee
+        )
+        project.organisation = organisation
+        project.frequency_of_session = request.data.get(
+            "frequency_of_session", project.frequency_of_session
+        )
+        project.interview_allowed = request.data.get(
+            "interview_allowed", project.interview_allowed
+        )
+        project.specific_coach = request.data.get(
+            "specific_coach", project.specific_coach
+        )
+        project.empanelment = request.data.get("empanelment", project.empanelment)
+        project.tentative_start_date = request.data.get(
+            "tentative_start_date", project.tentative_start_date
+        )
+        project.mode = request.data.get("mode", project.mode)
+        project.sold = request.data.get("sold", project.sold)
+        project.location = json.loads(request.data.get("location", "[]"))
+        project.project_description = request.data.get(
+            "project_description", project.project_description
+        )
         project.hr.clear()
         for hr in request.data["hr"]:
             single_hr = HR.objects.get(id=hr)
             project.hr.add(single_hr)
-        
+
         # Save the updated project
         project.save()
-        
+
         # You can return a success response with the updated project details
-        return Response({'message': 'Project updated successfully', 'project_id': project.id})
-    
+        return Response(
+            {"message": "Project updated successfully", "project_id": project.id}
+        )
+
     except Project.DoesNotExist:
-        return Response({'error': 'Project not found'}, status=404)
-    
+        return Response({"error": "Project not found"}, status=404)
+
     except Exception as e:
-        return Response({'error': str(e)}, status=500)
+        return Response({"error": str(e)}, status=500)
