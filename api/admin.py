ed1f995c46c2a5b72f6d698e4fa4d405821aa99d
Priyal-0410 <moviemela24901@gmail.com>
2023-10-17 11:22:07 +0530

    pmo analysis model serializer and view created

--- api/admin.py
+++ api/admin.py
@@ -15,6 +15,12 @@ from .models import (
     Goal,
     Competency,
     ActionItem,
+    ProfileEditActivity,
+    UserLoginActivity,
+    AddGoalActivity,
+    AddCoachActivity,
+    SentEmailActivity,
+
 )
 
 # Register your models here.
@@ -36,3 +42,8 @@ admin.site.register(Notification)
 admin.site.register(Goal)
 admin.site.register(Competency)
 admin.site.register(ActionItem)
+admin.site.register(ProfileEditActivity)
+admin.site.register(UserLoginActivity)
+admin.site.register(SentEmailActivity)
+admin.site.register(AddCoachActivity)
+admin.site.register(AddGoalActivity)
