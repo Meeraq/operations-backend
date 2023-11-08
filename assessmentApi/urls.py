4a51738a2d5538062e1737c52f9aa090c599fb36
Shashank Upadhyaya <moviemela24901@gmail.com>
2023-11-08 17:55:58 +0530

    api created for hr

--- assessmentApi/urls.py
+++ assessmentApi/urls.py
@@ -31,6 +31,9 @@ from .views import (
     GetParticipantObserversUniqueIds,
     StartAssessmentDisabled,
     ReleaseResults,
+    AssessmentsOfHr,
+    GetParticipantResponseForAllAssessment,
+    GetObserverResponseForAllAssessment,
 )
 
 
@@ -129,5 +132,18 @@ urlpatterns = [
     ),
     path(
         "release-assessment-result/<int:assessment_id>/",ReleaseResults.as_view(),
+    ),
+    path(
+        "assessments-of-hr/<str:hr_email>/",
+        AssessmentsOfHr.as_view(),
+    ),
+    path(
+        "get-participants-response-result-for-all-assessment/<str:hr_email>/",
+        GetParticipantResponseForAllAssessment.as_view(),
+    ),
+   
+    path(
+        "get-observer-assessment-response-result-for-all-assessment/<str:hr_email>/",
+        GetObserverResponseForAllAssessment.as_view(),
     ),
 ]
