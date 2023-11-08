7feecb8f9f68d1b67b6576369f63e7b1f6d8e09c
Manash Madhukar <91650744+8bit-Manash@users.noreply.github.com>
2023-11-08 17:41:23 +0530

    assessment labels

--- assessmentApi/views.py
+++ assessmentApi/views.py
@@ -570,6 +570,7 @@ class QuestionsForAssessment(APIView):
                 full_question = {
                     "id": question.id,
                     "self_question": question.self_question,
+                    "label": question.label,
                     "rating_type": question.rating_type,
                 }
 
