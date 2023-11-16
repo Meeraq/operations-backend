ad4f6cbd20335bd37bf255da3ff46ccd5798a904
pankaj-prog <wadhwanip38@gmail.com>
2023-11-16 15:02:30 +0530

    Live lesson and Text lesson Form

--- courses/models.py
+++ courses/models.py
@@ -14,3 +14,68 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Lesson(models.Model):
+    LESSON_TYPES = (
+        ("text", "Text Lesson"),
+        ("quiz", "Quiz Lesson"),
+        ("live_session", "Live Session"),
+        ("laser_coaching", "Laser Coaching Session"),
+        ("feedback", "Feedback"),
+        ("assessment", "Assessment"),
+        ("video", "Video"),
+    )
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("public", "Public"),
+    )
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    lesson_type = models.CharField(max_length=20, choices=LESSON_TYPES)
+
+
+class TextLesson(models.Model):
+    lesson = models.OneToOneField(Lesson, on_delete=models.CASCADE)
+    content = models.TextField(default="", blank=True)
+
+
+# class QuizLesson(models.Model):
+#     lesson = models.OneToOneField(Lesson, on_delete=models.CASCADE)
+#     questions = models.TextField()
+#     answers = models.TextField()
+
+
+# class LiveSession(models.Model):
+#     lesson = models.OneToOneField(Lesson, on_delete=models.CASCADE)
+#     description = models.TextField()
+#     meeting_link = models.URLField()
+#     date = models.DateField()
+#     start_time = models.TimeField()
+#     end_time = models.TimeField()
+
+
+# class LaserCoachingSession(models.Model):
+#     lesson = models.OneToOneField(Lesson, on_delete=models.CASCADE)
+#     description = models.TextField()
+#     booking_link = models.URLField()
+
+
+# class Feedback(models.Model):
+#     lesson = models.OneToOneField(Lesson, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=255)
+#     questions = (
+#         models.TextField()
+#     )  # JSONField or separate models for questions can be considered
+
+
+# class Assessment(models.Model):
+#     lesson = models.OneToOneField(Lesson, on_delete=models.CASCADE)
+#     message = models.TextField()
+
+
+# class Video(models.Model):
+#     lesson = models.OneToOneField(Lesson, on_delete=models.CASCADE)
+#     video = models.FileField(upload_to="videos/")
