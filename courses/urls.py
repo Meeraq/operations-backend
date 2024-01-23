506b162c30d9bb522383ae625243397a3cc912a5
Manash Madhukar <91650744+8bit-Manash@users.noreply.github.com>
2024-01-23 23:10:54 +0530

    consolidated reports task

--- courses/urls.py
+++ courses/urls.py
@@ -212,7 +212,12 @@ urlpatterns = [
     path("quizes/<int:quiz_id>/report/", views.get_quiz_report),
     path("quizes/<int:quiz_id>/report/download/", views.quiz_report_download),
     path("feedbacks/report/all/", views.get_all_feedbacks_report),
+    path("consolidated-feedback-report/", views.get_consolidated_feedback_report),
     path("feedbacks/<int:feedback_id>/report/", views.get_feedback_report),
+    path(
+        "get-consolidated-feedback-report-response/<int:lesson_id>/report/",
+        views.get_consolidated_feedback_report_response,
+    ),
     path(
         "get-laser-coaching-time/<int:laser_coaching_id>/<str:participant_email>/",
         GetLaserCoachingTime.as_view(),
