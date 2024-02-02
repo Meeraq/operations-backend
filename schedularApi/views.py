aa13036a0db34c6274de68981b709faa0da4cc9c
Shashank Upadhyaya <moviemela24901@gmail.com>
2024-02-02 15:50:41 +0530

    GetAllBatchesCoachDetails, GetAllBatchesParticipantDetails updated

--- schedularApi/views.py
+++ schedularApi/views.py
@@ -3534,7 +3534,7 @@ class GetAllBatchesCoachDetails(APIView):
             batches = SchedularBatch.objects.filter(project__id=project_id)
             all_coaches = []
             all_facilitator = []
-            # Iterate through batches and collect coaches
+
             for batch in batches:
                 for coach in batch.coaches.all():
                     coach_data = {
@@ -3542,7 +3542,7 @@ class GetAllBatchesCoachDetails(APIView):
                         "first_name": coach.first_name,
                         "last_name": coach.last_name,
                         "email": coach.email,
-                        "batchName": batch.name,
+                        "batchNames": [batch.name],
                         "phone": coach.phone,
                     }
                     all_coaches.append(coach_data)
@@ -3552,34 +3552,35 @@ class GetAllBatchesCoachDetails(APIView):
                         "first_name": facilitator.first_name,
                         "last_name": facilitator.last_name,
                         "email": facilitator.email,
-                        "batchName": batch.name,
+                        "batchNames": [batch.name],
                         "phone": facilitator.phone,
                     }
                     all_facilitator.append(facilitator_data)
 
-            # Remove duplicate coaches based on 'id'
-            unique_coaches = []
-            seen_ids = set()
-
+            unique_coaches = {}
             for coach_data in all_coaches:
                 coach_id = coach_data["id"]
-                if coach_id not in seen_ids:
-                    seen_ids.add(coach_id)
-                    unique_coaches.append(coach_data)
+                if coach_id not in unique_coaches:
+                    unique_coaches[coach_id] = coach_data
+                else:
+                    unique_coaches[coach_id]["batchNames"].extend(
+                        coach_data["batchNames"]
+                    )
 
-            unique_facilitator = []
-            facilitator_seen_ids = set()
-
+            unique_facilitator = {}
             for facilitator_data in all_facilitator:
                 facilitator_id = facilitator_data["id"]
-                if facilitator_id not in facilitator_seen_ids:
-                    facilitator_seen_ids.add(facilitator_id)
-                    unique_facilitator.append(facilitator_data)
+                if facilitator_id not in unique_facilitator:
+                    unique_facilitator[facilitator_id] = facilitator_data
+                else:
+                    unique_facilitator[facilitator_id]["batchNames"].extend(
+                        facilitator_data["batchNames"]
+                    )
 
             return Response(
                 {
-                    "unique_coaches": unique_coaches,
-                    "unique_facilitator": unique_facilitator,
+                    "unique_coaches": list(unique_coaches.values()),
+                    "unique_facilitator": list(unique_facilitator.values()),
                 },
                 status=status.HTTP_200_OK,
             )
@@ -3596,31 +3597,28 @@ class GetAllBatchesParticipantDetails(APIView):
     def get(self, request, project_id):
         try:
             batches = SchedularBatch.objects.filter(project__id=project_id)
-            all_learners = []
 
-            # Iterate through batches and collect coaches
+            learner_data_dict = {}
+
             for batch in batches:
                 for learner in batch.learners.all():
-                    learner_data = {
-                        "id": learner.id,
-                        "name": learner.name,
-                        "email": learner.email,
-                        "batchName": batch.name,
-                        "phone": learner.phone,
-                    }
-                    all_learners.append(learner_data)
+                    learner_id = learner.id
+                    if learner_id not in learner_data_dict:
+                        learner_data_dict[learner_id] = {
+                            "id": learner_id,
+                            "name": learner.name,
+                            "email": learner.email,
+                            "batchNames": [
+                                batch.name
+                            ],  # Initialize with list containing batch name
+                            "phone": learner.phone,
+                        }
+                    else:
+                        learner_data_dict[learner_id]["batchNames"].append(batch.name)
 
-            # Remove duplicate coaches based on 'id'
-            unique_learner = []
-            seen_ids = set()
+            unique_learner_data = list(learner_data_dict.values())
 
-            for learner_data in all_learners:
-                learner_id = learner_data["id"]
-                if learner_id not in seen_ids:
-                    seen_ids.add(learner_id)
-                    unique_learner.append(learner_data)
-
-            return Response(unique_learner, status=status.HTTP_200_OK)
+            return Response(unique_learner_data, status=status.HTTP_200_OK)
 
         except Exception as e:
             print(str(e))
